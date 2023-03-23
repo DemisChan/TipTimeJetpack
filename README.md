f6bff671d232e77c99c1fde22726d17263d6ccda
Demis Lavrentidis <demilavr@gmail.com>
2023-03-23 12:50:34 +0000

    -State hoisted to TipTimeScreen -placeholder for tip in string resource

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ To do this, we build a tip calculator app called Tip Time with these built-in Co
 * A Spacer composable to display empty space between the UI elements.
 
 <img alt="img.png" height="650" src="Screenshots/img.png" width="300"/>
+
+When Using States we will learn how to use state hoisting to expose state to higher function.
+
+## Without exposure
+<img alt="img_1.png" height="300" src="Screenshots/img_1.png" width="300"/>
+
+## With exposure
+<img alt="img_2.png" height="300" src="Screenshots/img_2.png" width="300"/>
--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
+import java.time.temporal.TemporalAmount
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,16 +40,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun EditNumberField(){
-    TextField(value = "", onValueChange = {})
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = Modifier
+            .fillMaxWidth(),
+        label = { Text(stringResource(id = R.string.cost_of_service)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+    )
+}
+
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0
+): String {
+    val tip = amount * tipPercent / 100
+    return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Composable
 fun TipTimeScreen() {
+    var amountInput by remember { mutableStateOf("") }
+
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
+    ) {
         Text(
             text = stringResource(id = R.string.calculate_tip),
             fontSize = 24.sp,
@@ -53,7 +82,19 @@ fun TipTimeScreen() {
         Spacer(
             modifier = Modifier.height(16.dp)
         )
-        EditNumberField()
+        EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it }
+        )
+        Spacer(
+            modifier = Modifier.height(24.dp)
+        )
+        Text(
+            text = stringResource(R.string.tip_amount, tip),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
     }
 }
 
